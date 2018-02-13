e6085dffae8249d7f993a5afbc0044da1b743831
AldenSharp <aldensharp2@gmail.com>
2018-02-13 13:19:13 -0600

    Compiled phonology information into a phonology object.

---
+++ src/main/java/model/Phonology.java
@@ -0,0 +1,39 @@
+package model;
+
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+import lombok.Builder;
+import lombok.Data;
+import model.descendantLanguage.DescendantLanguage;
+import model.prosody.Prosody;
+import model.syllableCondition.SyllableCondition;
+import model.writingSystem.WritingSystem;
+import util.ExceptionUtils;
+import util.TypeUtils;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import static util.FieldType.*;
+import static util.FieldType.OBJECT;
+
+@Data
+@Builder
+public class Phonology {
+    private List<List<PhonotacticsPosition>> phonotactics;
+    private Integer vowelCore;
+    private Prosody prosody;
+
+    public static Phonology getFromItem(Map<String, AttributeValue> item, String location) {
+        ExceptionUtils.checkObjectElements(
+                Arrays.asList("phonotactics", "vowelCore", "prosody"),
+                Arrays.asList(LIST, NUMBER, OBJECT),
+                location + " phonology", item
+        );
+        return Phonology.builder()
+                .phonotactics(PhonotacticsPosition.getListListFromItemList(item.get("phonotactics").getL(), location + ": phonotactics"))
+                .vowelCore(Integer.parseInt(item.get("vowelCore").getN()))
+                .prosody(Prosody.getFromItem(item.get("prosody").getM(), location + ": prosody"))
+                .build();
+    }
+}
--- src/main/java/model/Syngloss.java
+++ src/main/java/model/Syngloss.java
@@ -21,25 +21,21 @@ import static util.FieldType.*;
 public class Syngloss {
     private String name;
     private Integer date;
-    private List<List<PhonotacticsPosition>> phonotactics;
-    private Integer vowelCore;
-    private Prosody prosody;
+    private Phonology phonology;
     private SyllableCondition validity;
     private List<WritingSystem> writingSystems;
     private List<DescendantLanguage> descendantLanguages;
 
     public static Syngloss getFromItem(Map<String, AttributeValue> item) {
         ExceptionUtils.checkObjectElements(
-                Arrays.asList("name", "date", "phonotactics", "vowelCore", "prosody", "validity"),
-                Arrays.asList(STRING, NUMBER, LIST, NUMBER, OBJECT, OBJECT),
+                Arrays.asList("name", "date", "phonology", "validity"),
+                Arrays.asList(STRING, NUMBER, OBJECT, OBJECT),
                 "Syngloss", item
         );
         return Syngloss.builder()
                 .name(TypeUtils.getStringFromItem(item.get("name")))
                 .date(TypeUtils.getIntegerFromItem(item.get("date")))
-                .phonotactics(PhonotacticsPosition.getListListFromItemList(item.get("phonotactics").getL(), "Syngloss phonotactics"))
-                .vowelCore(Integer.parseInt(item.get("vowelCore").getN()))
-                .prosody(Prosody.getFromItem(item.get("prosody").getM(), "Syngloss prosody"))
+                .phonology(Phonology.getFromItem(item.get("phonology").getM(), "Syngloss phonology"))
                 .validity(SyllableCondition.getFromItem(item.get("validity").getM(), "Syngloss validity"))
                 .build();
     }

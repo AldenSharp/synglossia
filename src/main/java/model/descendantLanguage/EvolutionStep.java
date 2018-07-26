ff1e0835bca9e1300ce95499c2f78cb5b15e33a3
AldenSharp <aldensharp2@gmail.com>
2018-07-26 02:39:32 -0500

    Added description field to evolution step object.

--- src/main/java/model/descendantLanguage/EvolutionStep.java
+++ src/main/java/model/descendantLanguage/EvolutionStep.java
@@ -11,31 +11,31 @@ import lombok.Data;
 import util.ExceptionUtils;
 import util.TypeUtils;
 
-import static util.FieldType.LIST;
-import static util.FieldType.NUMBER;
-import static util.FieldType.OBJECT;
+import static util.FieldType.*;
 
 @Data
 @Builder
 public class EvolutionStep {
-	private Integer date;
-	private List<Transformation> transformations;
+    private Integer date;
+    private String description;
+    private List<Transformation> transformations;
 
-	private static EvolutionStep getFromItem(Map<String, AttributeValue> item, String location) {
-		ExceptionUtils.checkObjectElements(
-		        Arrays.asList("date", "transformations"),
-                Arrays.asList(NUMBER, LIST),
+    private static EvolutionStep getFromItem(Map<String, AttributeValue> item, String location) {
+        ExceptionUtils.checkObjectElements(
+                Arrays.asList("date", "description", "transformations"),
+                Arrays.asList(NUMBER, STRING, LIST),
                 location, item);
-		String locationWithDate = location + ", dated " + item.get("date").getN();
-	    return EvolutionStep.builder()
+        String locationWithDate = location + ", dated " + item.get("date").getN();
+        return EvolutionStep.builder()
                 .date(TypeUtils.getIntegerFromItem(item.get("date")))
+                .description(TypeUtils.getStringFromItem(item.get("description")))
                 .transformations(Transformation.getListFromItemList(item.get("transformations").getL(), locationWithDate + ": transformation"))
                 .build();
     }
 
     public static List<EvolutionStep> getListFromItemList(List<AttributeValue> itemList, String location) {
-		ExceptionUtils.checkListElements(itemList, location, OBJECT);
-	    return itemList.stream()
+        ExceptionUtils.checkListElements(itemList, location, OBJECT);
+        return itemList.stream()
                 .map(item -> getFromItem(item.getM(), location + " at position " + itemList.indexOf(item)))
                 .collect(Collectors.toList());
     }

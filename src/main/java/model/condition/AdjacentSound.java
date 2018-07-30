dfa2ca3cdabfbd277811fb67dd529eb40a9117f1
AldenSharp <aldensharp2@gmail.com>
2018-07-29 23:34:16 -0500

    BEFORE and AFTER conditions take list of values.

--- src/main/java/model/condition/AdjacentSound.java
+++ src/main/java/model/condition/AdjacentSound.java
@@ -7,24 +7,26 @@ import util.ExceptionUtils;
 import util.TypeUtils;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
+import static util.FieldType.LIST;
 import static util.FieldType.STRING;
 
 @Data
 @Builder
 public class AdjacentSound {
 	private AdjacentSoundType type;
-	private String value;
+	private List<String> values;
 
 	public static AdjacentSound getFromItem(Map<String, AttributeValue> item, String location) {
         ExceptionUtils.checkObjectElements(
-        		Arrays.asList("type", "value"),
-                Arrays.asList(STRING, STRING),
+        		Arrays.asList("type", "values"),
+                Arrays.asList(STRING, LIST),
                 location, item);
 	    return AdjacentSound.builder()
                 .type(AdjacentSoundType.getFromItem(item.get("type")))
-                .value(TypeUtils.getStringFromItem(item.get("value")))
+                .values(TypeUtils.getStringListFromItemList(item.get("values").getL(), location + ": values"))
                 .build();
     }
 }

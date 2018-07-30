0450252bee0ff183e0fe1940737210e7ff74cd37
AldenSharp <aldensharp2@gmail.com>
2018-07-30 18:42:25 -0500

    Added dates to descendant languages.

--- src/main/java/model/descendantLanguage/DescendantLanguage.java
+++ src/main/java/model/descendantLanguage/DescendantLanguage.java
@@ -12,29 +12,32 @@ import util.ExceptionUtils;
 import util.TypeUtils;
 
 import static util.FieldType.LIST;
+import static util.FieldType.NUMBER;
 import static util.FieldType.STRING;
 
 @Data
 @Builder
 public class DescendantLanguage {
-	private String name;
-	private List<EvolutionStep> evolution;
-	private List<DescendantLanguage> descendantLanguages;
-	
-	public static DescendantLanguage getFromItem(Map<String, AttributeValue> item) {
-		ExceptionUtils.checkObjectElements(
-		        Arrays.asList("name", "evolution"),
-                Arrays.asList(STRING, LIST),
+    private String name;
+    private Integer date;
+    private List<EvolutionStep> evolution;
+    private List<DescendantLanguage> descendantLanguages;
+
+    public static DescendantLanguage getFromItem(Map<String, AttributeValue> item) {
+        ExceptionUtils.checkObjectElements(
+                Arrays.asList("name", "date", "evolution"),
+                Arrays.asList(STRING, NUMBER, LIST),
                 "Descendant language", item);
         String locationWithName = "Descendant language '" + item.get("name").getS() + "'";
-		return DescendantLanguage.builder()
-				.name(TypeUtils.getStringFromItem(item.get("name")))
-				.evolution(EvolutionStep.getListFromItemList(item.get("evolution").getL(), locationWithName + " evolution step"))
+        return DescendantLanguage.builder()
+                .name(TypeUtils.getStringFromItem(item.get("name")))
+                .date(TypeUtils.getIntegerFromItem(item.get("date")))
+                .evolution(EvolutionStep.getListFromItemList(item.get("evolution").getL(), locationWithName + " evolution step"))
                 .descendantLanguages(getRecursiveDescendantLanguages(TypeUtils.getStringFromItem(item.get("name"))))
-				.build();
-	}
+                .build();
+    }
 
     private static List<DescendantLanguage> getRecursiveDescendantLanguages(String languageName) {
-	    return new LanguageService().getDescendantLanguagesFromData(languageName);
+        return new LanguageService().getDescendantLanguagesFromData(languageName);
     }
 }

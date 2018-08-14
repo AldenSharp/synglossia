772d1766658bfe311432e7e2d9e155db3d2b5e97
AldenSharp <aldensharp2@gmail.com>
2018-08-14 11:26:56 -0500

    Added support for sesquisyllables.

--- src/main/java/model/Phonology.java
+++ src/main/java/model/Phonology.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import model.prosody.Prosody;
 import util.ExceptionUtils;
+import util.TypeUtils;
 
 import java.util.Arrays;
 import java.util.List;
@@ -18,19 +19,19 @@ import static util.FieldType.OBJECT;
 public class Phonology {
     private List<List<PhonotacticsPosition>> phonotactics;
     private Syllable incrementingSyllable;
-    private Integer vowelCore;
+    private List<Integer> syllableCores;
     private Prosody prosody;
 
     public static Phonology getFromItem(Map<String, AttributeValue> item, String location) {
         ExceptionUtils.checkObjectElements(
-                Arrays.asList("phonotactics", "vowelCore", "incrementingSyllable", "prosody"),
-                Arrays.asList(LIST, NUMBER, OBJECT, OBJECT),
+                Arrays.asList("phonotactics", "syllableCores", "incrementingSyllable", "prosody"),
+                Arrays.asList(LIST, LIST, OBJECT, OBJECT),
                 location + " phonology", item
         );
         return Phonology.builder()
                 .phonotactics(PhonotacticsPosition.getListListFromItemList(item.get("phonotactics").getL(), location + ": phonotactics"))
                 .incrementingSyllable(Syllable.getFromItem(item.get("incrementingSyllable").getM(), location + ": incrementing syllable"))
-                .vowelCore(Integer.parseInt(item.get("vowelCore").getN()))
+                .syllableCores(TypeUtils.getIntegerListFromItemList(item.get("syllableCores").getL(), location + ": syllable cores"))
                 .prosody(Prosody.getFromItem(item.get("prosody").getM(), location + ": prosody"))
                 .build();
     }

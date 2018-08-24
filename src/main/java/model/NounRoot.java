d29bcc2a7a3687b85932727efb131741067f3dcc
AldenSharp <aldensharp2@gmail.com>
2018-08-24 01:05:25 -0500

    Added gender to noun root.

--- src/main/java/model/NounRoot.java
+++ src/main/java/model/NounRoot.java
@@ -3,6 +3,7 @@ package model;
 import com.amazonaws.services.dynamodbv2.model.AttributeValue;
 import lombok.Builder;
 import lombok.Data;
+import model.grammar.Gender;
 import util.ExceptionUtils;
 import util.TypeUtils;
 
@@ -18,16 +19,19 @@ import static util.FieldType.STRING;
 public class NounRoot {
     private List<List<String>> phonology;
     private String nounClass;
+    private Gender gender;
 
     public static NounRoot getFromItem(Map<String, AttributeValue> item) {
+        item.computeIfAbsent("gender", key -> new AttributeValue().withS(Gender.COMMON.name()));
         ExceptionUtils.checkObjectElements(
-                Arrays.asList("phonology", "class"),
-                Arrays.asList(LIST, STRING),
+                Arrays.asList("phonology", "class", "gender"),
+                Arrays.asList(LIST, STRING, STRING),
                 "Noun root", item
         );
         return NounRoot.builder()
                 .phonology(TypeUtils.getStringListListFromItemList(item.get("phonology").getL(), "Noun root phonology"))
                 .nounClass(TypeUtils.getStringFromItem(item.get("class")))
+                .gender(Gender.getFromItem(item.get("gender")))
                 .build();
     }
 }
